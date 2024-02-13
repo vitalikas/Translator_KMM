0ac0e4e29975ea2a679c1dccceecbf240beedab9
Vitalijus <vitalijus.kolinko@develappers.de>
2024-02-13 17:20:57 +0200

    UiLanguage added

---
+++ shared/src/commonMain/kotlin/lt/vitalijus/translator_kmm/core/presentation/UiLanguage.kt
@@ -0,0 +1,12 @@
+package lt.vitalijus.translator_kmm.core.presentation
+
+import lt.vitalijus.translator_kmm.core.domain.language.Language
+
+expect class UiLanguage {
+
+    val language: Language
+    companion object {
+        fun byCode(langCode: String): UiLanguage
+        val allLanguages: List<UiLanguage>
+    }
+}
---
+++ shared/src/commonMain/kotlin/lt/vitalijus/translator_kmm/translate/data/history/SqlDelightHistoryDataSource.kt
@@ -0,0 +1,43 @@
+package lt.vitalijus.translator_kmm.translate.data.history
+
+import app.cash.sqldelight.coroutines.asFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.datetime.Clock
+import lt.vitalijus.translator_kmm.TranslateDatabase
+import lt.vitalijus.translator_kmm.core.domain.util.CommonFlow
+import lt.vitalijus.translator_kmm.core.domain.util.toCommonFlow
+import lt.vitalijus.translator_kmm.translate.domain.history.HistoryDataSource
+import lt.vitalijus.translator_kmm.translate.domain.history.HistoryItem
+
+class SqlDelightHistoryDataSource(
+    db: TranslateDatabase
+) : HistoryDataSource {
+
+    private val queries = db.translateQueries
+
+    override fun getHistory(): CommonFlow<List<HistoryItem>> {
+        return queries
+            .getHistory()
+            .asFlow()
+            .map { query ->
+                query.executeAsList()
+            }
+            .map { history ->
+                history.map { historyEntity ->
+                    historyEntity.toHistoryItem()
+                }
+            }
+            .toCommonFlow()
+    }
+
+    override suspend fun insertHistoryItem(item: HistoryItem) {
+        return queries.insertHistoryEntity(
+            id = item.id,
+            fromLanguageCode = item.fromLanguageCode,
+            fromText = item.fromText,
+            toLanguageCode = item.toLanguageCode,
+            toText = item.toText,
+            timestamp = Clock.System.now().toEpochMilliseconds()
+        )
+    }
+}
