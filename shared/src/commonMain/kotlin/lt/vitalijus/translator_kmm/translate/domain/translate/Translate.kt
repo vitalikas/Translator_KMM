749be28ed06e21d08ce164ea44dab5b95e1412f4
Vitalijus <vitalijus.kolinko@develappers.de>
2024-02-13 17:21:35 +0200

    insert and translate use cases added

---
+++ shared/src/commonMain/kotlin/lt/vitalijus/translator_kmm/translate/domain/translate/Translate.kt
@@ -0,0 +1,28 @@
+package lt.vitalijus.translator_kmm.translate.domain.translate
+
+import lt.vitalijus.translator_kmm.core.domain.language.Language
+import lt.vitalijus.translator_kmm.core.domain.util.Resource
+
+class Translate(
+    private val client: TranslateClient
+) {
+
+    suspend fun execute(
+        fromLanguage: Language,
+        fromText: String,
+        toLanguage: Language
+    ): Resource<String> {
+        return try {
+            val translatedText = client.translate(
+                fromLanguage = fromLanguage,
+                fromText = fromText,
+                toLanguage = toLanguage
+            )
+
+            Resource.Success(data = translatedText)
+        } catch (e: TranslateException) {
+            e.printStackTrace()
+            Resource.Error(throwable = e)
+        }
+    }
+}
