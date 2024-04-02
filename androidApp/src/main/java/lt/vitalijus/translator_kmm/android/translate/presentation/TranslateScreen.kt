4789e4387075fe0abbe1767529b0e90bb190fb0f
Vitalijus <vitalijus.kolinko@develappers.de>
2024-04-02 10:51:53 +0300

    cleanup

--- androidApp/src/main/java/lt/vitalijus/translator_kmm/android/translate/presentation/TranslateScreen.kt
+++ androidApp/src/main/java/lt/vitalijus/translator_kmm/android/translate/presentation/TranslateScreen.kt
@@ -14,22 +14,18 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material.FabPosition
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.pulltorefresh.PullToRefreshContainer
 import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -84,9 +80,6 @@ fun TranslateScreen(
         }
     }
 
-    var isSpeaking by remember {
-        mutableStateOf(false)
-    }
     val tts = rememberTextToSpeech(
         onDispose = {
             onEvent(TranslateEvent.StopSpeakingText)
@@ -96,11 +89,10 @@ fun TranslateScreen(
 
         setOnUtteranceProgressListener(object : UtteranceProgressListener() {
             override fun onStart(utteranceId: String?) {
-                isSpeaking = true
+
             }
 
             override fun onDone(utteranceId: String?) {
-                isSpeaking = false
                 onEvent(TranslateEvent.StopSpeakingText)
             }
 
